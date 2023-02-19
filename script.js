84ddd2f44c1b761c3935422997e19564a2c93a68
ayush2342 <ayushsharma2342@gmail.com>
2023-02-19 15:04:32 +0530

    New Project

---
+++ script.js
@@ -0,0 +1,68 @@
+"use scrict";
+
+var buttonClick=document.getElementById("startGame");
+var firstDiv=document.querySelector(".first-Image");
+var SecondDiv=document.querySelector(".second-image");
+var gameStart=document.querySelector(".Startgame");
+
+// Image1= Scissor
+// Image2=Rock
+// Image3=Paper
+
+
+function displayDivs()
+{
+    firstDiv.classList.remove('hidden');
+    SecondDiv.classList.remove('hidden');
+    gameStart.classList.remove('hidden');
+
+    var value1=Math.trunc(Math.random()*3)+1;
+    var value2=Math.trunc(Math.random()*3)+1;
+
+    console.log(value1,value2)
+
+    firstDiv.style.backgroundImage = `url(image${value1}.png)`;
+    SecondDiv.style.backgroundImage = `url(image${value2}.png)`;
+    if(value1==value2)
+    {
+        gameStart.innerHTML="Game drawn - Try Again";
+    }
+    else if(value1==1)
+    {
+        if(value2==2)
+        {
+            gameStart.innerHTML="Player 2 Wins"
+        }
+        else
+        {
+            gameStart.innerHTML="Player 1 Wins"
+        }
+        
+    }
+    else if(value1==2)
+    {
+        if(value2==1)
+        {
+            gameStart.innerHTML="Player 1 Wins"
+        }
+        else
+        {
+            gameStart.innerHTML="Player 2 Wins"
+        }
+    }
+    else
+    {
+        if(value2==1)
+        {
+            gameStart.innerHTML="Player 2 Wins"
+        }
+        else
+        {
+            gameStart.innerHTML="Player 1 Wins"
+        }
+    }
+
+}
+buttonClick.addEventListener("click",displayDivs);
+
+
